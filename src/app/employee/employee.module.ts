f30a46d40ab131bbc13b0adb708e172c905991b2
Harsh Patel <harsh3608@gmail.com>
2023-06-30 17:48:12 +0530

    added account service

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatIconModule} from '@angular/material/icon'
+import { HttpClientModule } from '@angular/common/http';
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { LoginComponent } from './login/login.component';
+import { ToastrModule } from 'ngx-toastr';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -11,7 +18,16 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     CommonModule,
-    EmployeeRoutingModule
+    EmployeeRoutingModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot(),
   ]
 })
 export class EmployeeModule { }
--- src/app/employee/login/login.component.ts
+++ src/app/employee/login/login.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  isLoading: boolean = false;
+  loginForm!: FormGroup;
+  showPassword: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<LoginComponent>,
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      Email: new FormControl('', [Validators.required, Validators.email]),
+      Password: new FormControl('', [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{6,15}')])
+    });
+  }
+
+  SubmitloginForm(){}
+
+  public togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  CloseDialog() {
+    this.dialogRef.close();
+  }
+
+
+
+  //Getter functions to get for-values from form-controls
+  get Email(): FormControl {
+    return this.loginForm.get("Email") as FormControl;
+  }
+  get Password(): FormControl {
+    return this.loginForm.get("Password") as FormControl;
+  }
 
 }
