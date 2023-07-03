4d62d542c71d4640423af64a058f35b20f39206a
Harsh Patel <harsh3608@gmail.com>
2023-07-03 11:01:12 +0530

    completed guards and interceptor

--- src/app/employee/shared/authorization/interceptor/auth-config.interceptor.ts
+++ src/app/employee/shared/authorization/interceptor/auth-config.interceptor.ts
@@ -6,13 +6,36 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class AuthConfigInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService,
+  ) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    if (this.authService.isLoggedIn() && !this.authService.isRequestAuthorized()) {
+      // redirect to login page
+      this.authService.removeToken();
+      this.router.navigate(['/']);
+      this.toastr.error('User Session Expired! Please, Login to continue !', 'Logged Out', {
+        timeOut: 2000,
+      });
+    }
+
+    const authToken = this.authService.getToken();
+    request = request.clone({
+      setHeaders: {
+        Authorization: "Bearer " + authToken
+      }
+    });
     return next.handle(request);
+
   }
 }
