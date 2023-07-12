9610d47a851763983186e85ed5b09b2a47f36040
Harsh Patel <harsh3608@gmail.com>
2023-07-12 17:50:33 +0530

    .

--- src/app/employee/leave-add-dialog/leave-add-dialog.component.ts
+++ src/app/employee/leave-add-dialog/leave-add-dialog.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/authorization/auth.service';
 
 @Component({
   selector: 'app-leave-add-dialog',
   templateUrl: './leave-add-dialog.component.html',
   styleUrls: ['./leave-add-dialog.component.css']
 })
-export class LeaveAddDialogComponent {
+export class LeaveAddDialogComponent implements OnInit{
+  AddLeaveRequestForm!: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private dialog: MatDialog,
+    private fb: FormBuilder
+  ){}
+
+  ngOnInit(): void {
+    
+  }
 
 }
