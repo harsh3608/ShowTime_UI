a295fd2eb8f3dbd7fc6cd5ea9abe469d492d2fbd
Harsh Patel <harsh3608@gmail.com>
2023-07-21 10:38:18 +0530

    fixed bugs-1

--- src/app/employee/leave-manager/leave-manager.component.ts
+++ src/app/employee/leave-manager/leave-manager.component.ts
@@ -17,19 +17,19 @@ export class LeaveManagerComponent implements OnInit{
   allLeaveRequests:LeaveDTO[]=[];
   userLeaveRequests:LeaveDTO[]=[];
 
-  first1: number = 0;
-  rows1: number = 10;
-  first2: number = 0;
-  rows2: number = 10;
-  first3: number = 0;
-  rows3: number = 10;
-  totalRecords: number = 120;
-  options = [
-    { label: 5, value: 5 },
-    { label: 10, value: 10 },
-    { label: 20, value: 20 },
-    { label: 120, value: 120 }
-  ];
+  // first1: number = 0;
+  // rows1: number = 10;
+  // first2: number = 0;
+  // rows2: number = 10;
+  // first3: number = 0;
+  // rows3: number = 10;
+  // totalRecords: number = this.allLeaveRequests.length;
+  // options = [
+  //   { label: 5, value: 5 },
+  //   { label: 10, value: 10 },
+  //   { label: 20, value: 20 },
+  //   { label: 120, value: 120 }
+  // ];
 
   constructor(
     private authService: AuthService,
@@ -98,20 +98,20 @@ export class LeaveManagerComponent implements OnInit{
 
 
 
-onPageChange1(event: PageEvent) {
-    this.first1 = event.first;
-    this.rows1 = event.rows;
-}
+// onPageChange1(event: PageEvent) {
+//     this.first1 = event.first;
+//     this.rows1 = event.rows;
+// }
 
-onPageChange2(event: PageEvent) {
-    this.first2 = event.first;
-    this.rows2 = event.rows;
-}
+// onPageChange2(event: PageEvent) {
+//     this.first2 = event.first;
+//     this.rows2 = event.rows;
+// }
 
-onPageChange3(event: PageEvent) {
-    this.first3 = event.first;
-    this.rows3 = event.rows;
-}
+// onPageChange3(event: PageEvent) {
+//     this.first3 = event.first;
+//     this.rows3 = event.rows;
+// }
 
 
 
